74f001f6755e5b947282dd391741a5987c66c23f
Mg5393 <628mg42@canonslade.tbft.uk>
2026-09-16 09:25:43 +0000

    new work

--- Class Exercises/2 - Javascript Selection/Task 1d - Whats the Number/script.js
+++ Class Exercises/2 - Javascript Selection/Task 1d - Whats the Number/script.js
@@ -10,8 +10,10 @@ document.addEventListener('DOMContentLoaded', function() {
 // Function to check if a number is positive, negative, or zero
 function checkNumber() {
     // TODO: Get the number from the input field
-    
+    let number = parseInt(document.getElementById('number').value);
     // TODO: Check if the number is positive, negative, or zero
-    
+    if (number < 0) {
+        document.getElementById('result').textContent = "Your number is negative"
+    } if 
     // TODO: Display the appropriate message (POSITIVE, NEGATIVE, or ZERO)
 }
